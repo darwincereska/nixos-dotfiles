64518e24b5faa874a435d3c519b67591cc993b21
darwincereska <accfordarwin@gmail.com>
2024-09-08 00:56:02 -0400

    Added ArmCord, Micro, and Home Manager

---
+++ private_dot_config/home-manager/home.nix
@@ -0,0 +1,49 @@
+{ conifg, pkgs, ...}: {
+	imports = [
+		# ./modules/pywal.nix
+		
+	];
+	nixpkgs = {
+	    config = {
+	    allowUnfree = true;
+	    packageOverrides = pkgs: {
+	    # unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
+	    };
+	  };
+	 };
+	
+	home = {
+		username = "darwin";
+		homeDirectory = "/home/darwin";
+		stateVersion = "24.05";
+		
+		packages = with pkgs; [
+			btop # Monitor
+			htop # Monitor
+			armcord # Lighweight Discord Client
+			chezmoi # Dotfiles Manager
+			fzf  # Searches Files
+			python3 # Python			
+			dialog
+			libguestfs
+			pywal
+			cava # Music Visualizer
+			vscode # Code Editor
+			android-tools # ADB Tools
+			prismlauncher # Multi MC Launcher
+			authenticator # Gnome Authenticator
+			neovim # Text Editor
+			spotify # Music Client
+			endeavour # Google Tasks
+			beeper # Chat Client
+			firefox-wayland # Firefox
+			fastfetch  # Neofetch but fast
+	    	localsend
+    		mission-center # Task Manager
+	    	chromium
+	    	scrcpy # Android Screen Mirror
+		];
+		
+	
+	};
+}
